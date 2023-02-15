7cd61407bc07a3c84cde0a678c17a93bb7e589ac
Your Name <th599396@gmail.com>
2023-02-15 18:19:48 +0700

    quanly

---
+++ src/student/quanlysinhvien.java
@@ -0,0 +1,15 @@
+package student;
+
+public class quanlysinhvien {
+    private String fullname;
+    private Integer tuôi;
+    private String ma_sinh_viên;
+    private String phone;
+    public class quanlysinhvien(String fullname,Integer tuôi ,String ma_sinh_viên, String phone){
+        this.fullname=
+
+    }
+
+
+
+}
